015e233eab761a3c6c9d71b0aab7de68086a15c0
Sigrit <sigrit.sults@tktk.ee>
2022-11-23 23:51:37 +0200

    Week5Operators

---
+++ ANDRanges/Program.cs
@@ -0,0 +1,36 @@
+﻿//temp võrdne 0 freezing cold
+//temp 0 kuni 10 siis cold
+// temp 10 kuni 15 chilly
+//15 kuni 20 warm
+//20 kuni 30 hot
+// 30 peale boiling hot
+
+
+Console.WriteLine("Enter the temperature:");
+int temp = Int32.Parse(Console.ReadLine());
+
+if (temp >= 30)
+{
+    Console.WriteLine("Boiling hot.");
+}
+
+else if (temp < 30 && temp >= 20)
+{
+    Console.WriteLine("Hot.");
+}
+else if (temp < 20 && temp >= 15)
+{
+    Console.WriteLine("Warm.");
+}
+else if (temp < 15 && temp >= 10)
+{
+    Console.WriteLine("Chilly.");
+}
+else if (temp < 10 && temp > 0)
+{
+    Console.WriteLine("Cold.");
+}
+else
+{
+    Console.WriteLine("Freezing cold.");
+}
---
+++ AndOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿//90 või rohkem tulemus
+//&& mõlemad tingimused peavad tõesed olema siis tulem tõene
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology= Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chemistry= Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congrats! You got accepted!");
+}
+else
+{
+    Console.WriteLine("You are not enough!");
+}
